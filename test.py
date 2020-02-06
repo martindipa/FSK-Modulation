48f7cd3867a4b0b481ceda9e033667c7b963fb02
Karitham <57440386+Karitham@users.noreply.github.com>
2020-02-06 23:35:14 +0100

    affichage de sinNum aussi

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from lib import binToNum, bintransfo, filtre, huffman, msg, signalMod, wavWrite, openOutput, demodBin
 
-message = msg('C:\\Users\\ploui\\Documents\\A1\\Bloc4\\GitHub\\FSK-Modulation\\message.txt')
+message = msg('message.txt')
 asciiBin = bintransfo(message)
 huff = huffman(message)
 print(f'message huffman : {huff}')
-#sinNum = binToNum(huff)
+sinNum = binToNum(huff)
 sinMod = signalMod(huff)
 FFTi = filtre(sinMod, huff)
 wavWrite(FFTi,'output.wav')
